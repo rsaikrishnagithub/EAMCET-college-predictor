0fc6b5be4fea9a1e09e69d813bd5e1ede737c35a
Sricharan <vodnalasricharan@gmail.com>
2021-09-09 18:09:04 +0530

    updated dataset

--- ECP_app/views.py
+++ ECP_app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
+	if request.method == 'POST':
+		rank=request.POST.get('rank')
+		gender=request.POST.get('gender')
+		caste=request.POST.get('caste')
+		branch=request.POST.get('branch')
+		
 	return render(request,'homepagee.html')
