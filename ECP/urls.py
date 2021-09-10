57c394960295cc6c35e35b0e491d98a4d8e7e973
Sricharan <vodnalasricharan@gmail.com>
2021-09-10 17:20:08 +0530

    final commit

--- ECP/urls.py
+++ ECP/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('ECP_app.urls')),
+]
+urlpatterns += [
+
+  url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
+  url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
